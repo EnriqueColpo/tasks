6e4a99448ad0b90e8d2151ffa9812c99d524995c
EnriqueColpo <EnriqueColpo@gmail.com>
2024-06-16 21:37:52 -0300

    update cors

--- services/tasks_api/main.py
+++ services/tasks_api/main.py
@@ -1,3 +1,4 @@
+import os
 import uuid
 from typing import Union
 
@@ -17,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origins=[os.getenv("ALLOWED_ORIGINS")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
