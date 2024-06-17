0cedbf901ad4f0e82a8bae203d2c696006fbd076
EnriqueColpo <EnriqueColpo@gmail.com>
2024-06-17 13:48:35 -0300

    chango cors

--- services/tasks_api/main.py
+++ services/tasks_api/main.py
@@ -13,12 +13,13 @@ from models import Task
 from schemas import APITask, APITaskList, CloseTask, CreateTask
 from store import TaskStore
 
+
 app = FastAPI()
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("ALLOWED_ORIGINS")],
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
