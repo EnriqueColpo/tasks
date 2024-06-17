5542e196e6f9cc2526822377b65d5944c5aee652
EnriqueColpo <EnriqueColpo@gmail.com>
2024-06-16 21:32:59 -0300

    fix user pool config

--- services/tasks_ui/src/aws-exports.js
+++ services/tasks_ui/src/aws-exports.js
@@ -1,7 +1,9 @@
 export default {
     Auth: {
         region: 'eu-west-1',
-        userPoolId: 'eu-west-1_3wYZ5Ed8u',
-        userPoolWebClientId: '5dh0uhv3ka9htpbi3gkmi7ggpr'
+        userPoolId: 'eu-west-1_Zntk4DfY1',
+        userPoolWebClientId: '3imts4sdgodt65ef6ohvc0kphp'
     }
 };
+
+// todo: fix this
